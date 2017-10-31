51955b600530e827a0021530153b1a7eb6a69729
gppayawal <gppayawal@gmail.com>
2017-10-31 14:00:49 +0800

    exer1 with time

---
+++ lab01.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int * column_sum(int **m, int n){
+	int row, col;
+	int *v;
+
+	v = (int *)malloc(n*sizeof(int));
+
+	
+	for(row = 0; row < n; row++){
+		v[row] = 0;
+		for(col = 0; col < n; col++){
+			v[row] = v[row] + m[col][row];
+		}
+	}
+	return v;
+}
+
+void main(){
+	int n;	//size of square matrix
+	int **m; //main matrix
+	int *v;	//array of column sums
+	int i, row, col;
+
+	printf("Enter size of matrix: ");
+	scanf("%d", &n);
+
+	//Allocate memory space for the main matrix given nxn dimension
+	m = (int **)malloc(n*sizeof(int *));
+	for(i = 0; i < n; i++){
+		m[i] = (int *)malloc(n*sizeof(int));
+	}
+
+	//Allocate memory space for array of column sums
+	v = (int *)malloc(n*sizeof(int));
+	for(i = 0; i < n; i++){
+		v[i] = 0;
+	}
+
+	//Generate random number for the matrix
+	srand(time(NULL));
+	for(row = 0; row < n; row++){
+		for(col = 0; col < n; col++){
+			m[row][col] = rand() % 10;
+		}
+	}
+
+	/* Matrix print
+	for(row = 0; row < n; row++){
+		for(col = 0; col < n; col++){
+			printf("%3d", m[row][col]);
+		}
+		printf("\n");
+	}*/
+
+	clock_t begin = clock();
+	v = column_sum(m, n);
+	clock_t end =  clock();
+	double elapsed = (double) (end - begin) / CLOCKS_PER_SEC;	//column_sum timing
+
+	printf("Elapsed: %f sec\n", elapsed);
+}
